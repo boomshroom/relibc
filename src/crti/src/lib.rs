34e24a1453f4666b462335fae4843141afc6a7e8
Angelo Bulfone <mbulfone@gmail.com>
2018-04-04 23:19:49 -0700

    Add aarch64 implementation of crti and crtn from musl.

--- src/crti/src/lib.rs
+++ src/crti/src/lib.rs
@@ -4,6 +4,7 @@
 #![feature(global_asm)]
 #![feature(lang_items)]
 
+#[cfg(target_arch = "x86_64")]
 global_asm!(r#"
     .section .init,"ax",@progbits
     .global _init
@@ -18,6 +19,24 @@ global_asm!(r#"
     _fini:
     	push %rbp
     	movq %rsp, %rbp
+"#);
+
+// Shamelessly stolen from musl
+#[cfg(target_arch = "aarch64")]
+global_asm!(r#"
+    .section .init,"ax",@progbits
+    .global _init
+    .type _init, @function
+    _init:
+        stp x29,x30,[sp,-16]!
+        mov x29,sp
+
+    .section .fini,"ax",@progbits
+    .global _fini
+    .type _fini, @function
+    _fini:
+        stp x29,x30,[sp,-16]!
+        mov x29,sp
 "#);
 
 #[lang = "panic_fmt"]
--- src/crtn/src/lib.rs
+++ src/crtn/src/lib.rs
@@ -4,6 +4,8 @@
 #![feature(global_asm)]
 #![feature(lang_items)]
 
+// Shamelessly stolen from the OSDev wiki
+#[cfg(target_arch = "x86_64")]
 global_asm!(r#"
     .section .init,"ax",@progbits
         popq %rbp
@@ -11,6 +13,18 @@ global_asm!(r#"
 
     .section .fini,"ax",@progbits
         popq %rbp
+        ret
+"#);
+
+// Shamelessly stolen from musl
+#[cfg(target_arch = "aarch64")]
+global_asm!(r#"
+    .section .init,"ax",@progbits
+        ldp x29,x30,[sp],#16
+        ret
+
+    .section .fini,"ax",@progbits
+        ldp x29,x30,[sp],#16
         ret
 "#);
 
